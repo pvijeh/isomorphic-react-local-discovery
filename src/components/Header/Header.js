98db8f7cb24f52ddf7a4843421734afa38393428
peter vijeh <peter@peters-MacBook-Pro.local>
2015-11-25 16:39:14 -0500

    working on header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,15 +13,11 @@ class Header extends Component {
     return (
       <div className="Header">
         <div className="Header-container">
-          <a className="Header-brand" href="/" onClick={Link.handleClick}>
-            <img className="Header-brandImg" src={require('./logo-small.png')} width="38" height="38" alt="React" />
-            <span className="Header-brandTxt">Your Company</span>
-          </a>
-          <Navigation className="Header-nav" />
-          <div className="Header-banner">
-            <h1 className="Header-bannerTitle">React</h1>
-            <p className="Header-bannerDesc">Complex web apps made easy</p>
-          </div>
+          <a href="/" onClick={Link.handleClick}>ABOUT</a>
+          <a href="/" onClick={Link.handleClick}>HOME</a>
+          <a className="Header-Logo" href="/" onClick={Link.handleClick}></a>
+          <a href="/" onClick={Link.handleClick}>LOGIN</a>
+          <a href="/" onClick={Link.handleClick}>JOIN</a>
         </div>
       </div>
     );
--- src/components/VenueProfile/VenueProfile.js
+++ src/components/VenueProfile/VenueProfile.js
@@ -3,6 +3,7 @@
 import React, { PropTypes, Component } from 'react';
 import withStyles from '../../decorators/withStyles';
 import styles from './VenueProfile.css';
+import Link from '../Link';
 
 @withStyles(styles)
 class VenueProfile extends Component {
@@ -20,7 +21,8 @@ class VenueProfile extends Component {
     const venue = 'Venue Name';
     const reviewSectionOne = 'Why People Like ';
     const reviewSectionTwo = 'What Kind of Crowd Goes to ';
-    const reviewSectionThree = ' Appears on These Best of Lists';
+    const reviewSectionThree = 'What Kind of Crowd Goes to ';
+    const reviewSectionFour = ' Appears on These Best of Lists';
 
     // not sure what this context stuff is about,  need to research it 
 
@@ -32,44 +34,81 @@ class VenueProfile extends Component {
     return (
       <div className="VenueProfile">
         <div className="VenueProfile-container">
+          
           <div className="VenueProfile-MainImage">
             <h1>{venue}</h1>
             main image goes here,  not sure if I will keep a large hero image 
           </div>
-          <div className="VenueProfile-ReviewSection">
-            {reviewSectionOne+venue}
-          </div>
-          <div className="VenueProfile-ReviewSection">
-            {reviewSectionTwo+venue}
-          </div>
-          <div className="VenueProfile-ReviewSection">
-            {venue+reviewSectionThree}
+          
+        <div className="VenueProfile-ReviewSection">
+          <h3>{reviewSectionOne+venue}</h3>
+          <div className="VenueProfile-BestListItemRow">
+            <div className="VenueProfile-BestListItemColumn">
 
-<div className="VenueProfile-BestListItemRow">
-  
-  <div className="VenueProfile-BestListItemColumn">
-    <div className="VenueProfile-BestListItem"></div>
-    <div className="VenueProfile-BestListItem"></div>
-    <div className="VenueProfile-BestListItem"></div>
-    <div className="VenueProfile-BestListItem"></div>
-  </div>
-  
-  <div className="VenueProfile-BestListItemColumn">
-    <div className="VenueProfile-BestListItem"></div>
-    <div className="VenueProfile-BestListItem"></div>
-    <div className="VenueProfile-BestListItem"></div>
-    <div className="VenueProfile-BestListItem"></div>
-  </div>
-
-</div>
-
-          </div>
-            <div className="VenueProfile-ImageContainer">
-                <div className="VenueProfile-Image">image</div>
-                <div className="VenueProfile-Image">image</div>
-                <div className="VenueProfile-Image">image</div>
+              <div className="VenueProfile-BestListItem">
+                <a href="/about" onClick={Link.handleClick}></a>
+                <div className="VenueProfile-ListItemVoteContainer">
+                  <span>1111</span>
+                </div>
+              </div>
+              
+              <div className="VenueProfile-BestListItem"></div>
+              <div className="VenueProfile-BestListItem"></div>
+              <div className="VenueProfile-BestListItem"></div>
             </div>
-          <p>...</p>
+            <div className="VenueProfile-BestListItemColumn">
+              <div className="VenueProfile-BestListItem"></div>
+              <div className="VenueProfile-BestListItem"></div>
+              <div className="VenueProfile-BestListItem"></div>
+              <div className="VenueProfile-BestListItem"></div>
+            </div>
+          </div>    
+        </div>
+          
+          <div className="VenueProfile-ReviewSection">
+            <div className="VenueProfile-BestListItemRow">
+              <div className="VenueProfile-BestListItemColumn">
+              <h3>{venue+reviewSectionFour}</h3>
+                <div className="VenueProfile-BestListItem"></div>
+                <div className="VenueProfile-BestListItem"></div>
+                <div className="VenueProfile-BestListItem"></div>
+                <div className="VenueProfile-BestListItem"></div>
+              </div>
+
+              <div className="VenueProfile-BestListItemColumn">
+              <h3>{venue+reviewSectionFour}</h3>
+                <div className="VenueProfile-BestListItem"></div>
+                <div className="VenueProfile-BestListItem"></div>
+                <div className="VenueProfile-BestListItem"></div>
+                <div className="VenueProfile-BestListItem"></div>
+              </div>
+            </div>
+          </div>
+
+        <div className="VenueProfile-ReviewSection">
+          <h3>{reviewSectionOne+venue}</h3>
+          <div className="VenueProfile-BestListItemRow">
+            <div className="VenueProfile-BestListItemColumn">
+              <div className="VenueProfile-BestListItem"></div>
+              <div className="VenueProfile-BestListItem"></div>
+              <div className="VenueProfile-BestListItem"></div>
+              <div className="VenueProfile-BestListItem"></div>
+            </div>
+            <div className="VenueProfile-BestListItemColumn">
+              <div className="VenueProfile-BestListItem"></div>
+              <div className="VenueProfile-BestListItem"></div>
+              <div className="VenueProfile-BestListItem"></div>
+              <div className="VenueProfile-BestListItem"></div>
+            </div>
+          </div>    
+        </div>
+
+          <div className="VenueProfile-ImageContainer VenueProfile-BestListItemRow">
+              <div className="VenueProfile-Image">image</div>
+              <div className="VenueProfile-Image">image</div>
+              <div className="VenueProfile-Image">image</div>
+          </div>
+
         </div>
       </div>
     );
